4ea834e28cc20b933dc37bf7cf9473a2ac2618c6
L0mchansky <persmax13@gmail.com>
2026-03-19 01:07:44 +0300

    add task_2

---
+++ Assets/Scripts/m27_28_task_2/TimerModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace m27_28_task_2
+{
+    public class TimerModel
+    {
+        public event Action TimerTick;
+        public event Action TimerReset;
+
+        private bool _isRunning;
+        private float _remainingTime;
+        private float _fullTime;
+
+        public TimerModel(float fullTime)
+        {
+            _fullTime = fullTime;
+            _remainingTime = fullTime;
+        }
+
+        public bool IsRunning => _isRunning;
+        public float RemainingTime => _remainingTime;
+        public float FullTime => _fullTime;
+
+        public void Tick(float deltaTime)
+        {
+            if (_isRunning == false) return;
+
+            _remainingTime = _remainingTime - deltaTime;
+
+            if (_remainingTime < 0)
+            {
+                _remainingTime = 0;
+                Stop();
+            }
+
+            TimerTick?.Invoke();
+        }
+
+        public void Start()
+        {
+            _isRunning = true;
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+        }
+
+        public void Reset()
+        {
+            _isRunning = false;
+            _remainingTime = _fullTime;
+            TimerReset?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/m27_28_task_2/TimerView/TimerView.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace m27_28_task_2
+{
+    public abstract class TimerView: MonoBehaviour
+    {
+        private TimerModel _timer;
+        protected TimerModel Timer => _timer;
+
+        private void Start()
+        {
+            FillTimerText();
+        }
+
+        public void Initialize(TimerModel timer)
+        {
+            _timer = timer;
+
+            _timer.TimerTick += OnTimerTick;
+            _timer.TimerReset += OnTimerReset;
+        }
+
+        private void OnDestroy()
+        {
+            _timer.TimerTick -= OnTimerTick;
+        }
+
+        private void OnTimerTick()
+        {
+            ViewRender();
+        }
+
+        private void OnTimerReset()
+        {
+            ViewReset();
+        }
+
+        protected abstract void FillTimerText();
+        protected abstract void ViewRender();
+        protected abstract void ViewReset();
+    }
+}
